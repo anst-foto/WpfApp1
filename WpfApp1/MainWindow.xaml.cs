7ba2d5a58156fa1815bbebf2af1a707af964b64b
Starinin Andrey <starininandrey@gmail.com>
2023-08-15 18:21:47 +0300

    Initial commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var progress = new Progress<int>();
+            progress.ProgressChanged += (_, i) =>
+            {
+                ProgressBar.Value = i;
+                Values.Text = i.ToString();
+            };
+            await ProcessAsync(progress, (int)ProgressBar.Minimum, (int)ProgressBar.Maximum);
+        }
+
+        private async Task ProcessAsync(IProgress<int> progress, int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                await Task.Delay(500);
+                progress.Report(i);
+            }
+        }
+    }
+}
